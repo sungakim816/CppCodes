58c95fea370b422b4b283e8e67ec940c56eeeafd
Kim Sunga <sungakim816@gmail.com>
2018-10-30 18:05:21 +0800

    Improved Presentation

--- Hangaman_Console/HangmanGame/Hangman.cpp
+++ Hangaman_Console/HangmanGame/Hangman.cpp
@@ -1,7 +1,8 @@
 #include "Hangman.h"
 
+
 std::string hide_word(std::string mystery_word){
-unsigned int index = 0, measure = 0;
+unsigned short index = 0, measure = 0;
 measure = mystery_word.length();
 for(index = 0; index < measure; index++){
         if (mystery_word[index] != ' '){
@@ -13,10 +14,15 @@ return mystery_word;
 }
 
 std::string mysteryCapital(std::string mystery_word){
-unsigned int index = 0, measure = 0;
+unsigned short index = 0, measure = 0;
 measure = mystery_word.length();
 for(; index < measure; index++){
     mystery_word[index] = toupper(mystery_word[index]);
 }
 return mystery_word;
+}
+
+bool isUserInputValid(std::string userInput){
+    if (userInput.length() > 1 || userInput.length() == 0 || userInput == " ") return false;
+    return true;
 }
--- Hangaman_Console/HangmanGame/main.cpp
+++ Hangaman_Console/HangmanGame/main.cpp
@@ -5,57 +5,68 @@
 #include <ctime>
 #include <cctype>
 #include <cstring>
+#include <sstream>
 #include "Hangman.h"
 using namespace std;
 
 int main(){
-string mystery_word0 = "", mystery_word1 = "", mystery_word2 = "", mystery_word1_copy = "", user_input = "", previous_inputs = "";
-int lifePoints = 5;
+string mystery_word = "", hidden_mystery_word = "", mystery_word_all_caps = "", user_input = "", previous_inputs = "";
+string alphabethLetters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", message = "";
+short lifePoints = 5;
 cout<<"Enter The Mystery Word: ";
-getline(cin, mystery_word0);
+getline(cin, mystery_word);
 
-mystery_word1 = hide_word(mystery_word0);
+hidden_mystery_word = hide_word(mystery_word);
 
-mystery_word2 = mysteryCapital(mystery_word0); // ALL CAPS MYSTERY WORD
+mystery_word_all_caps= mysteryCapital(mystery_word); // ALL CAPS MYSTERY WORD
 system("cls");
 
 for(;;){//main loop
-   cout<<endl<<mystery_word1<<endl;
+   cout<<endl<<hidden_mystery_word<<endl;
+   cout<<message;
+   cout<<endl<<alphabethLetters<<endl;
    cout<<endl<<"Previous Input/s: "<<previous_inputs<<endl<<endl;
    cout<<"Enter Your Guess: ";
-   cin>>user_input;
+   getline(cin, user_input);
 
-   if (user_input.length() == 1){
+   if (isUserInputValid(user_input)){
    user_input[0] = toupper(user_input[0]); // CHANGE TO UPPER CASE USER GUESS..
 
-    if (previous_inputs.find(user_input) == string::npos && mystery_word2.find(user_input) != string::npos){
+    if (previous_inputs.find(user_input) == string::npos && mystery_word_all_caps.find(user_input) != string::npos){
         previous_inputs += user_input;
-    for (unsigned int index = 0; index < mystery_word0.length(); index++){
+        alphabethLetters.replace(alphabethLetters.find(user_input), 1, "-");
+    for (unsigned int index = 0; index < mystery_word.length(); index++){
 
-        if (user_input[0] == mystery_word2[index]){ // CHECKS THE USER INPUT
+        if (user_input[0] == mystery_word_all_caps[index]){ // CHECKS THE USER INPUT
 
-            mystery_word1.replace(index, 1, mystery_word0.substr(index, 1)); // REPLACE IF THE USER GUESS EXITS IN THE MYSTERY  WORD
+            hidden_mystery_word.replace(index, 1, mystery_word.substr(index, 1)); // REPLACE IF THE USER GUESS EXITS IN THE MYSTERY WORD
         }
     }
-    if (mystery_word0 == mystery_word1){
-		cout<<"You Solved the Mystery Word which is "<< mystery_word0 << endl;
+    message = "";
+    if (mystery_word == hidden_mystery_word){
+		cout<<"You Solved the Mystery Word which is "<< mystery_word << endl;
         cout<<endl<<"   Congratulations....."<<endl<<"    Thanks For Playing. See You Next Time"<<endl;
         break;
     }
     }else if (previous_inputs.find(user_input) != string::npos){
-
-        cout<<endl<<user_input<<" is already been entered. Pls Try Again."<<endl;
-        cout<<lifePoints<<" life/lives remaining"<<endl;
-    }else if (previous_inputs.find(user_input) == string::npos && mystery_word2.find(user_input) == string::npos){
+        stringstream stream;
+        stream << "\n"<< user_input << " is already been entered. Pls try Again.\n" << lifePoints << " life/lives remaining.\n";
+        message = stream.str();
+    }else if (previous_inputs.find(user_input) == string::npos && mystery_word_all_caps.find(user_input) == string::npos){
         previous_inputs += user_input;
-        cout<<endl<<user_input<<" is not on the mystery word "<<endl;
-        cout<<--lifePoints<<" life/lives remaining"<<endl;
+        alphabethLetters.replace(alphabethLetters.find(user_input), 1, "-");
+        stringstream stream;
+        stream << "\n"<< user_input << " is not on the mystery word\n" << --lifePoints << " life/lives remaining.\n";
+        message = stream.str();
         if (lifePoints == 0){
-            cout<<endl<<"You have Zero Life Points "<<endl<<endl<<"The Mystery Word is: "<<mystery_word0;
+            cout<<endl<<"You have Zero Life Points "<<endl<<endl<<"The Mystery Word is: "<<mystery_word << "\n";
             break;
         }
     }
-   }else cout<<endl<<"Invalid Input, One Character At a Time"<<endl;
+   }else {
+        message = "\nInvalid Input, Please Try Again";
+    }
+   system("cls");
 }
 system("pause");
 return 0;
