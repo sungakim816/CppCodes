c872650105057fb47c7cdfe771f1803e3e4b3cd1
Kim Sunga <sungakim816@gmail.com>
2019-04-27 19:32:33 +0800

    Bug Fixed PrefixPostfix Library

--- PrefixPostfix/include/prefixpostfix.h
+++ PrefixPostfix/include/prefixpostfix.h
@@ -1,31 +1,31 @@
 #pragma once
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <locale>
 #include <sstream>
 #include <iterator>
+#include <algorithm>
 
 class PrefixPostfix
 {
 public:
-    PrefixPostfix();
-    PrefixPostfix(std::string);
-    ~PrefixPostfix();
-    std::string postfix();
-    std::string prefix();
-    void setEquation(std::string);
-    std::string getEquation() const { return equation; };
-    void clear();
-
+	PrefixPostfix();
+	// virtual ~PrefixPostfix();
+	PrefixPostfix(std::string);
+	~PrefixPostfix();
+	std::string postfix();
+	std::string prefix();
+	void setEquation(std::string);
+	std::string getEquation() const { return equation; };
+	void clear();
 private:
-    std::string equation;
-    std::ostringstream postoutput, preoutput;
-    const std::string postfixoperator = "*/+-(";
-    const std::string prefixoperator = "*/+-)";
-    const unsigned short operatorsvalue[5] = {2, 2, 1, 1, 0};
-    std::vector<char> operatorvector, postfixvector, prefixvector;
-    std::vector<char>::iterator itr;
+	std::string equation = "";
+	std::ostringstream postoutput, preoutput;
+	const std::string postfixoperator = "*/+-(";
+	const std::string prefixoperator = "*/+-)";
+	const unsigned short operatorsvalue[5] = { 2, 2, 1, 1, 0 };
+	std::vector<char> operatorvector, postfixvector, prefixvector;
 
 protected:
 };
+
--- PrefixPostfix/src/prefixpostfix.cpp
+++ PrefixPostfix/src/prefixpostfix.cpp
@@ -1,4 +1,4 @@
-#include "prefixpostfix.h"
+#include "PrefixPostfix.h"
 
 PrefixPostfix::PrefixPostfix()
 {
@@ -6,7 +6,7 @@ PrefixPostfix::PrefixPostfix()
 
 PrefixPostfix::PrefixPostfix(std::string s)
 {
-    this->equation = s;
+	this->equation = s;
 }
 
 PrefixPostfix::~PrefixPostfix()
@@ -15,111 +15,143 @@ PrefixPostfix::~PrefixPostfix()
 
 void PrefixPostfix::setEquation(std::string s)
 {
-    this->clear();
-    this->equation = s;
+	this->clear();
+	this->equation = s;
 }
 
 std::string PrefixPostfix::postfix()
 {
-    if (postoutput.str().length() > 0)
-    {
-        return postoutput.str();
-    }
-    for (unsigned int idx = 0; idx < equation.length(); idx++)
-    {
-        if (std::isalnum(equation[idx]))
-        {
-            //if equation[idx] is an alpha-numeric, push it to the postfixvector
-            postfixvector.push_back(equation[idx]);
-        }
-        else if ((equation[idx] == '(') || (postfixoperator.find(equation[idx]) != std::string::npos && operatorvector.empty()))
-        {
-            // if equation[idx] is a '(' push it to the operator stack;
-            operatorvector.push_back(equation[idx]);
-        }
-        else if (equation[idx] == ')')
-        {
-            // if equation[idx] is ')' push the content of operatorvector until '(' is found;
-            while (operatorvector.back() != '(')
-            {
-                postfixvector.push_back(operatorvector.back());
-                operatorvector.pop_back();
-            }
-            operatorvector.pop_back();
-        }
-        else if (postfixoperator.find(equation[idx]) != std::string::npos)
-        {
-            unsigned int oprvalue = operatorsvalue[postfixoperator.find(equation[idx])];
-            while (operatorsvalue[postfixoperator.find(operatorvector.back())] >= oprvalue)
-            {
-                postfixvector.push_back(operatorvector.back());
-                operatorvector.pop_back();
-            }
-            operatorvector.push_back(equation[idx]);
-        }
-    }
-    for (itr = operatorvector.end() - 1; itr >= operatorvector.begin(); itr--)
-    {
-        postfixvector.push_back(*itr);
-    }
-    operatorvector.clear();
-    std::copy(postfixvector.begin(), postfixvector.end(), std::ostream_iterator<char>(postoutput, ""));
-    return postoutput.str();
+	if (postoutput.str().length() > 0)
+	{
+		return postoutput.str();
+	}
+	for (unsigned int idx = 0; idx < equation.length(); idx++)
+	{
+		if (std::isalnum(equation[idx]))
+		{
+			//if equation[idx] is an alpha-numeric, push it to the postfixvector
+			postfixvector.push_back(equation[idx]);
+		}
+		else if ((equation[idx] == '(') || (postfixoperator.find(equation[idx]) != std::string::npos && operatorvector.empty()))
+		{
+			// if equation[idx] is a '(' push it to the operator stack;
+			operatorvector.push_back(equation[idx]);
+		}
+		else if (equation[idx] == ')')
+		{
+			// if equation[idx] is ')' push the content of operatorvector until '(' is found;
+			while (!operatorvector.empty())
+			{
+				if (operatorvector.back() != '(')
+				{
+					postfixvector.push_back(operatorvector.back());
+					operatorvector.pop_back();
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			if (!operatorvector.empty())
+				operatorvector.pop_back();
+		}
+		else if (postfixoperator.find(equation[idx]) != std::string::npos)
+		{
+			unsigned int oprvalue = operatorsvalue[postfixoperator.find(equation[idx])];
+			while (!operatorvector.empty())
+			{
+				if (operatorsvalue[postfixoperator.find(operatorvector.back())] >= oprvalue)
+				{
+					postfixvector.push_back(operatorvector.back());
+					operatorvector.pop_back();
+				}
+				else
+				{
+					break;
+				}
+			}
+			operatorvector.push_back(equation[idx]);
+		}
+	}
+	for (auto itr = operatorvector.rbegin(); itr != operatorvector.rend(); ++itr)
+	{
+		postfixvector.push_back(*itr);
+	}
+	operatorvector.clear();
+	std::copy(postfixvector.begin(), postfixvector.end(), std::ostream_iterator<char>(postoutput, ""));
+	return postoutput.str();
 }
 
 std::string PrefixPostfix::prefix()
 {
-    if (preoutput.str().length() > 0)
-    {
-        return preoutput.str();
-    }
-    for (int idx = equation.length() - 1; idx >= 0; idx--)
-    {
-        if (std::isalnum(equation[idx]))
-        {
-            prefixvector.insert(prefixvector.begin(), equation[idx]);
-        }
-        else if ((equation[idx] == ')') || (prefixoperator.find(equation[idx]) != std::string::npos && operatorvector.empty()))
-        {
-            operatorvector.push_back(equation[idx]);
-        }
-        else if (equation[idx] == '(')
-        {
-            while (operatorvector.back() != ')')
-            {
-                prefixvector.insert(prefixvector.begin(), operatorvector.back());
-                operatorvector.pop_back();
-            }
-            operatorvector.pop_back();
-        }
-        else if (prefixoperator.find(equation[idx]) != std::string::npos)
-        {
-            unsigned int oprvalue = operatorsvalue[prefixoperator.find(equation[idx])];
-            while (operatorsvalue[prefixoperator.find(operatorvector.back())] >= oprvalue)
-            {
-                prefixvector.insert(prefixvector.begin(), operatorvector.back());
-                operatorvector.pop_back();
-            }
-            operatorvector.push_back(equation[idx]);
-        }
-    }
-    for (itr = operatorvector.end() - 1; itr >= operatorvector.begin(); itr--)
-    {
-        prefixvector.insert(prefixvector.begin(), *itr);
-    }
-    operatorvector.clear();
-    std::copy(prefixvector.begin(), prefixvector.end(), std::ostream_iterator<char>(preoutput, ""));
-    return preoutput.str();
+	if (preoutput.str().length() > 0)
+	{
+		return preoutput.str();
+	}
+	unsigned int equationSize = equation.length();
+	for (int idx = equationSize - 1; idx >= 0; idx--)
+	{
+		if (std::isalnum(equation[idx]))
+		{
+			prefixvector.insert(prefixvector.begin(), equation[idx]);
+		}
+		else if ((equation[idx] == ')') || (prefixoperator.find(equation[idx]) != std::string::npos && operatorvector.empty()))
+		{
+			operatorvector.push_back(equation[idx]);
+		}
+		else if (equation[idx] == '(')
+		{
+			while (!operatorvector.empty())
+			{
+
+				if (operatorvector.back() != ')')
+				{
+					prefixvector.insert(prefixvector.begin(), operatorvector.back());
+					operatorvector.pop_back();
+				}
+				else
+					break;
+				
+			}
+			if (!operatorvector.empty())
+				operatorvector.pop_back();
+		}
+		else if (prefixoperator.find(equation[idx]) != std::string::npos)
+		{
+			unsigned int oprvalue = operatorsvalue[prefixoperator.find(equation[idx])];
+			while (!operatorvector.empty())
+			{
+
+				if (operatorsvalue[prefixoperator.find(operatorvector.back())] >= oprvalue)
+				{
+					prefixvector.insert(prefixvector.begin(), operatorvector.back());
+					operatorvector.pop_back();
+				}
+				else
+					break;		
+			}
+			if (!operatorvector.empty())
+				operatorvector.push_back(equation[idx]);
+		}
+	}
+	for (auto itr = operatorvector.rbegin(); itr != operatorvector.rend(); itr++)
+	{
+		prefixvector.insert(prefixvector.begin(), *itr);
+	}
+	operatorvector.clear();
+	std::copy(prefixvector.begin(), prefixvector.end(), std::ostream_iterator<char>(preoutput, ""));
+	return preoutput.str();
 }
 
 void PrefixPostfix::clear()
 {
-    postfixvector.clear();
-    prefixvector.clear();
-    operatorvector.clear();
-    postoutput.str(std::string());
-    postoutput.clear();
-    preoutput.str(std::string());
-    preoutput.clear();
-    equation = "";
+	postfixvector.clear();
+	prefixvector.clear();
+	operatorvector.clear();
+	postoutput.str(std::string());
+	postoutput.clear();
+	preoutput.str(std::string());
+	preoutput.clear();
+	equation = "";
 }
